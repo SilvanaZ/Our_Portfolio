78b3e2c6dbee995f266d0e44cdb9bd9d8240b256
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-09-06 21:20:28 -0300

    Protecting Routes

--- src/OurApp.jsx
+++ src/OurApp.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import { AppRouter } from '../src/routes';
+import { AppRouter } from './router';
 import './styles.css';
 
 export const OurApp = () => {
